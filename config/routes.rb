d3feba8feac5acb2909138dc3556dab751e6579b
agustinmar <agustin_mar@hotmail.com>
2016-03-03 16:57:37 -0600

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :libros
 
   #Ruta a página principal
-  root :to => "home#acerca"
+  root :to => "home#inicio"
 
   #Rutas opciones menú de navegación
   get 'home/inicio'
