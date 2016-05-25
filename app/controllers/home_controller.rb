f7f4bd0c80a9acbc79e930700cac2499345535b3
agustinmar <agustin_mar@hotmail.com>
2016-05-25 10:35:29 -0500

    Agregando recursos precompilados para Heroku

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -73,11 +73,11 @@ class HomeController < ApplicationController
     @autores = Autor.order("date DESC")
     if params[:texto].present?
       #Desarrollo
-      @titulos = Titulo.where("lower(nombre) LIKE ?", "%#{params[:texto].downcase}%")
-      @autores = Autor.where("lower(nombre) LIKE ?", "%#{params[:texto].downcase}%")
+      #@titulos = Titulo.where("lower(nombre) LIKE ?", "%#{params[:texto].downcase}%")
+      #@autores = Autor.where("lower(nombre) LIKE ?", "%#{params[:texto].downcase}%")
       #Producción
-      #@titulos = Titulo.where("translate(lower(nombre),'âãäåāăąÁÂÃÄÅĀĂĄèééêëēĕėęěĒĔĖĘĚìíîïìĩīĭÌÍÎÏÌĨĪĬóôõöōŏőÒÓÔÕÖŌŎŐùúûüũūŭůÙÚÛÜŨŪŬŮ','aaaaaaaaaaaaaaaeeeeeeeeeeeeeeeiiiiiiiiiiiiiiiiooooooooooooooouuuuuuuuuuuuuuuu') LIKE ?", "%#{ActiveSupport::Inflector.transliterate(params[:texto]).downcase}%")
-      #@autores = Autor.where("translate(lower(nombre),'âãäåāăąÁÂÃÄÅĀĂĄèééêëēĕėęěĒĔĖĘĚìíîïìĩīĭÌÍÎÏÌĨĪĬóôõöōŏőÒÓÔÕÖŌŎŐùúûüũūŭůÙÚÛÜŨŪŬŮ','aaaaaaaaaaaaaaaeeeeeeeeeeeeeeeiiiiiiiiiiiiiiiiooooooooooooooouuuuuuuuuuuuuuuu') LIKE ?", "%#{ActiveSupport::Inflector.transliterate(params[:texto]).downcase}%")
+      @titulos = Titulo.where("translate(lower(nombre),'âãäåāăąÁÂÃÄÅĀĂĄèééêëēĕėęěĒĔĖĘĚìíîïìĩīĭÌÍÎÏÌĨĪĬóôõöōŏőÒÓÔÕÖŌŎŐùúûüũūŭůÙÚÛÜŨŪŬŮ','aaaaaaaaaaaaaaaeeeeeeeeeeeeeeeiiiiiiiiiiiiiiiiooooooooooooooouuuuuuuuuuuuuuuu') LIKE ?", "%#{ActiveSupport::Inflector.transliterate(params[:texto]).downcase}%")
+      @autores = Autor.where("translate(lower(nombre),'âãäåāăąÁÂÃÄÅĀĂĄèééêëēĕėęěĒĔĖĘĚìíîïìĩīĭÌÍÎÏÌĨĪĬóôõöōŏőÒÓÔÕÖŌŎŐùúûüũūŭůÙÚÛÜŨŪŬŮ','aaaaaaaaaaaaaaaeeeeeeeeeeeeeeeiiiiiiiiiiiiiiiiooooooooooooooouuuuuuuuuuuuuuuu') LIKE ?", "%#{ActiveSupport::Inflector.transliterate(params[:texto]).downcase}%")
     end
     @categorias = Categoria.all
     @libros = Libro.all
