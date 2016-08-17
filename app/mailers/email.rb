3bb446b949ac49796ceb02094cd50f380dc60a9a
agustinmar <agustin_mar@hotmail.com>
2016-08-17 14:26:21 -0500

    Agregando recursos precompilados para Heroku

--- app/mailers/email.rb
+++ app/mailers/email.rb
@@ -7,7 +7,7 @@ class Email < ActionMailer::Base
   #   en.email.recepcion.subject
   #
   def micorreo(parametros)
-    enviar_correo(parametros,"agustin_mar@hotmail.com","Sección de Contacto Llevo Libros")
+    enviar_correo(parametros,"info@llevolibros.com","Sección de Contacto Llevo Libros")
   end
 
   def enviar(parametros)
