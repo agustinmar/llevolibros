b7bcecdc359b51f81ddee12cc5a3623abd0d2795
agustinmar <agustin_mar@hotmail.com>
2016-08-22 21:42:41 -0500

    Agregando recursos para Heroku

--- app/mailers/action_correo.rb
+++ app/mailers/action_correo.rb
@@ -1,15 +1,16 @@
 class ActionCorreo < ActionMailer::Base
   default from: "Llevo Libros <info@llevolibros.com>"
 
-  def recepcion
-  	@greeting = "Hola"
+  def recepcion(solicitud)
+    @solicitud = solicitud
+  	#@greeting = "Hola"
   	mail :to => "agustin.mar@gmail.com", :subject => "Solicitud Llevo Libros"
   end
 
-  def misolicitud(solicitud)
-    @solicitud = solicitud
-    mail :to => "agustin.mar@gmail.com", :subject => "Solicitud Llevo Libros"
-  end
+  #def misolicitud(solicitud)
+  #  @solicitud = solicitud
+  #  mail :to => "agustin.mar@gmail.com", :subject => "Solicitud Llevo Libros"
+  #end
 
   #def enviarespuesta(solicitud)
   #  enviar_solicitud(parametros,parametros[:correo],"Llevo Libros - Recibimos tu solicitud de libro")
